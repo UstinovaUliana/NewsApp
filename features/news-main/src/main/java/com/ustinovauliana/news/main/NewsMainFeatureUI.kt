0e5a4064acd994d6b100311c0d36211aef62c775
Uliana <mirrorrune21363@gmail.com>
2024-07-16 12:40:42 +0300

    extract article ui composables

--- features/news-main/src/main/java/com/ustinovauliana/news/main/NewsMainFeatureUI.kt
+++ features/news-main/src/main/java/com/ustinovauliana/news/main/NewsMainFeatureUI.kt
@@ -1,52 +1,48 @@
 package com.ustinovauliana.news.main
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.ustinovauliana.news.NewsTheme
+import com.ustinovauliana.news.main.ui.Articles
 
 @Composable
 fun NewsMainScreen() {
@@ -56,6 +52,7 @@ fun NewsMainScreen() {
 @Composable
 @Suppress("LongMethod")
 internal fun NewsMainScreen(newsViewModel: NewsMainViewModel) {
+
     var query: String by rememberSaveable { mutableStateOf("") }
     var showClearIcon by rememberSaveable { mutableStateOf(false) }
 
@@ -67,51 +64,13 @@ internal fun NewsMainScreen(newsViewModel: NewsMainViewModel) {
     val state by newsViewModel.state.collectAsState()
     // val currentState = state
     // newsViewModel.getAll()
+
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
             .padding(top = statusBarHeight.dp),
         topBar = {
-            TextField(
-                value = query,
-                onValueChange = { onQueryChanged: String ->
-                    query = onQueryChanged
-                    if (onQueryChanged.isNotEmpty()) {
-                        newsViewModel.search(query)
-                    } else {
-                        newsViewModel.getAll()
-                    }
-                },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Rounded.Search,
-                        tint = NewsTheme.colorScheme.onBackground,
-                        contentDescription = "Search Icon"
-                    )
-                },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        query = ""
-                        newsViewModel.getAll()
-                    }) {
-                        Icon(
-                            imageVector = Icons.Rounded.Clear,
-                            tint = MaterialTheme.colorScheme.onBackground,
-                            contentDescription = "Clear Icon"
-                        )
-                    }
-                },
-                maxLines = 1,
-                placeholder = {
-                    stringResource(R.string.hint_search_query)
-                },
-                textStyle = MaterialTheme.typography.titleMedium,
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = NewsTheme.colorScheme.background, shape = RectangleShape),
-            )
+            SearchField(query, newsViewModel)
         },
         content = { paddingValues ->
             Box(modifier = Modifier.padding(paddingValues)) {
@@ -122,6 +81,55 @@ internal fun NewsMainScreen(newsViewModel: NewsMainViewModel) {
         }
     )
 }
+
+@Composable
+private fun SearchField(
+    query: String,
+    newsViewModel: NewsMainViewModel
+) {
+    var query1 = query
+    TextField(
+        value = query1,
+        onValueChange = { onQueryChanged: String ->
+            query1 = onQueryChanged
+            if (onQueryChanged.isNotEmpty()) {
+                newsViewModel.search(query1)
+            } else {
+                newsViewModel.getAll()
+            }
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Rounded.Search,
+                tint = NewsTheme.colorScheme.onBackground,
+                contentDescription = "Search Icon"
+            )
+        },
+        trailingIcon = {
+            IconButton(onClick = {
+                query1 = ""
+                newsViewModel.getAll()
+            }) {
+                Icon(
+                    imageVector = Icons.Rounded.Clear,
+                    tint = MaterialTheme.colorScheme.onBackground,
+                    contentDescription = "Clear Icon"
+                )
+            }
+        },
+        maxLines = 1,
+        placeholder = {
+            stringResource(R.string.hint_search_query)
+        },
+        textStyle = MaterialTheme.typography.titleMedium,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = NewsTheme.colorScheme.background, shape = RectangleShape),
+    )
+}
+
 
 @Composable
 private fun NewsMainContent(currentState: State) {
@@ -139,89 +147,6 @@ private fun NewsMainContent(currentState: State) {
             Articles(articles = currentState.articles)
         }
     }
-}
-
-@Composable
-private fun Articles(
-    @PreviewParameter(ArticlesUIPreviewProvider::class, limit = 1) articles: List<ArticleUI>
-) {
-    LazyColumn {
-        items(articles) { article ->
-            key(article.id) {
-                Article(article)
-            }
-        }
-    }
-}
-
-@Composable
-internal fun Article(
-    @PreviewParameter(
-        ArticleUIPreviewProvider::class,
-        limit = 1
-    ) article: ArticleUI
-) {
-    var isExpanded by remember {
-        mutableStateOf(false)
-    }
-    Row(Modifier.padding(bottom = 4.dp)) {
-        article.imageUrl?.let { imageUrl ->
-            var isImageVisible by remember { mutableStateOf(true) }
-            if (isImageVisible) {
-                AsyncImage(
-                    model = imageUrl,
-                    onState = { state ->
-                        if (state is AsyncImagePainter.State.Error) {
-                            isImageVisible = false
-                        }
-                    },
-                    contentDescription = stringResource(R.string.content_dedsc_item_article_image),
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.size(100.dp)
-                )
-            }
-        }
-        Spacer(modifier = Modifier.size(8.dp))
-        Column(modifier = Modifier.padding(8.dp)) {
-            Text(
-                text = article.title ?: "",
-                style = NewsTheme.typography.headlineMedium,
-                maxLines = 1
-            )
-            Spacer(modifier = Modifier.size(8.dp))
-            Text(
-                text = article.description ?: "",
-                style = NewsTheme.typography.bodyMedium,
-                maxLines = if (isExpanded) Int.MAX_VALUE else 3,
-            )
-            Text(
-                modifier = Modifier.clickable {
-                    isExpanded = !isExpanded
-                },
-                text = if (!isExpanded) "..." else "hide"
-            )
-        }
-    }
-}
-
-@Suppress("MagicNumber")
-private class ArticleUIPreviewProvider : PreviewParameterProvider<ArticleUI> {
-
-    override val values = sequenceOf(
-        ArticleUI(1, "Title1", "Description1", null, ""),
-        ArticleUI(2, "Title2", "Description2", null, ""),
-        ArticleUI(3, "Title3", "Description3", null, ""),
-        ArticleUI(4, "Title4", "Description4", null, ""),
-        ArticleUI(5, "Title5", "Description5", null, ""),
-    )
-}
-
-private class ArticlesUIPreviewProvider : PreviewParameterProvider<List<ArticleUI>> {
-
-    private val articleProvider = ArticleUIPreviewProvider()
-    override val values = sequenceOf(
-        articleProvider.values.toList()
-    )
 }
 
 @Suppress("UNUSED_PARAMETER")
---
+++ features/news-main/src/main/java/com/ustinovauliana/news/main/ui/ArticleListContent.kt
@@ -0,0 +1,113 @@
+package com.ustinovauliana.news.main.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import com.ustinovauliana.news.NewsTheme
+import com.ustinovauliana.news.main.ArticleUI
+import com.ustinovauliana.news.main.R
+
+@Composable
+internal fun Articles(
+    @PreviewParameter(ArticlesUIPreviewProvider::class, limit = 1) articles: List<ArticleUI>
+) {
+    LazyColumn {
+        items(articles) { article ->
+            key(article.id) {
+                Article(article)
+            }
+        }
+    }
+}
+
+@Composable
+internal fun Article(
+    @PreviewParameter(
+        ArticleUIPreviewProvider::class,
+        limit = 1
+    ) article: ArticleUI
+) {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    Row(
+        Modifier.padding(bottom = 4.dp)
+    ) {
+        article.imageUrl?.let { imageUrl ->
+            var isImageVisible by remember { mutableStateOf(true) }
+            if (isImageVisible) {
+                AsyncImage(
+                    model = imageUrl,
+                    onState = { state ->
+                        if (state is AsyncImagePainter.State.Error) {
+                            isImageVisible = false
+                        }
+                    },
+                    contentDescription = stringResource(R.string.content_dedsc_item_article_image),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(100.dp)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.size(8.dp))
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(
+                text = article.title ?: "",
+                style = NewsTheme.typography.headlineMedium,
+                maxLines = 1
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = article.description ?: "",
+                style = NewsTheme.typography.bodyMedium,
+                maxLines = if (isExpanded) Int.MAX_VALUE else 3,
+            )
+            Text(
+                modifier = Modifier.clickable {
+                    isExpanded = !isExpanded
+                },
+                text = if (!isExpanded) "..." else "hide"
+            )
+        }
+    }
+}
+
+@Suppress("MagicNumber")
+private class ArticleUIPreviewProvider : PreviewParameterProvider<ArticleUI> {
+
+    override val values = sequenceOf(
+        ArticleUI(1, "Title1", "Description1", null, ""),
+        ArticleUI(2, "Title2", "Description2", null, ""),
+        ArticleUI(3, "Title3", "Description3", null, ""),
+        ArticleUI(4, "Title4", "Description4", null, ""),
+        ArticleUI(5, "Title5", "Description5", null, ""),
+    )
+}
+
+private class ArticlesUIPreviewProvider : PreviewParameterProvider<List<ArticleUI>> {
+
+    private val articleProvider = ArticleUIPreviewProvider()
+    override val values = sequenceOf(
+        articleProvider.values.toList()
+    )
+}
